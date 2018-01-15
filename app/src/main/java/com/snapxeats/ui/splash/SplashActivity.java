428ee436b1f3c37abe648ed7bebd92de98c4f1e1
Durgesh Trivedi <durgesh.trivedi@synerzip.com>
2018-01-15 19:50:04 +0530

    Hockey app integration part 1

--- app/src/main/java/com/snapxeats/ui/splash/SplashActivity.java
+++ app/src/main/java/com/snapxeats/ui/splash/SplashActivity.java
@@ -8,6 +8,9 @@ import android.support.v7.app.AppCompatActivity;
 import com.snapxeats.R;
 import com.snapxeats.ui.login.LoginActivity;
 
+import net.hockeyapp.android.CrashManager;
+import net.hockeyapp.android.UpdateManager;
+
 /**
  * Created by Snehal Tembare on 12/1/18.
  */
@@ -24,5 +27,41 @@ public class SplashActivity extends AppCompatActivity {
             startActivity(new Intent(SplashActivity.this, LoginActivity.class));
             finish();
         }, TIME_OUT);
+
+        checkForUpdates();
     }
+    
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // ... your own onResume implementation
+        checkForCrashes();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterManagers();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterManagers();
+    }
+
+    private void checkForCrashes() {
+        CrashManager.register(this);
+    }
+
+    private void checkForUpdates() {
+        // Remove this for store builds!
+        UpdateManager.register(this);
+    }
+
+    private void unregisterManagers() {
+        UpdateManager.unregister();
+    }
+
 }
