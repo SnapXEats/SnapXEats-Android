ddb869230cb7637fc7c79e5e3213575e34b612bb
snehal.tembare <snehal.tembare@synerzip.com>
2018-07-16 16:37:20 +0530

    Remove hardcoded values for Check In

--- app/src/main/java/com/snapxeats/ui/home/HomeInteractor.java
+++ app/src/main/java/com/snapxeats/ui/home/HomeInteractor.java
@@ -262,8 +262,6 @@ public class HomeInteractor {
     }
 
     void getNearByRestaurantToCheckIn(double lat, double lng) {
-        lat = 40.7014;
-        lng = -74.0151;
 
         if (NetworkUtility.isNetworkAvailable(mContext)) {
             ApiHelper apiHelper = Objects.requireNonNull(ApiClient.getClient(mContext, BASE_URL)).create(ApiHelper.class);
