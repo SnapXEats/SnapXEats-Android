57cec395dd431db57fd3a063a022ca78e5ed3008
snehal.tembare <snehal.tembare@synerzip.com>
2018-10-09 14:11:40 +0530

    Remove hardcoded values

--- app/src/main/java/com/snapxeats/ui/home/HomeInteractor.java
+++ app/src/main/java/com/snapxeats/ui/home/HomeInteractor.java
@@ -262,8 +262,8 @@ public class HomeInteractor {
     }
 
     void getNearByRestaurantToCheckIn(double lat, double lng) {
-        lat = 40.7014;
-        lng = -74.0151;
+//        lat = 40.7014;
+//        lng = -74.0151;
         if (NetworkUtility.isNetworkAvailable(mContext)) {
             ApiHelper apiHelper = Objects.requireNonNull(ApiClient.getClient(mContext, BASE_URL)).create(ApiHelper.class);
             Call<CheckInRestaurants> checkInRestaurantsCall = apiHelper.getRestaurantsForCheckIn(lat,lng);
